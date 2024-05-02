5cd09d0cd8c59f54a367e50592f50fda4ed3bee6
Rocio Garrido <rocio.garrido@wefox.com>
2024-05-02 13:28:37 +0200

    feat: required input

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, booleanAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-title',
@@ -7,7 +7,11 @@ import { Component } from '@angular/core';
   imports: [
     CommonModule,
   ],
-  templateUrl: './title.component.html',
+  template: `<h1 class="text-3xl mb-5">{{ title }}</h1>`,
   styles: ``
 })
-export class TitleComponent { }
+export class TitleComponent {
+
+  @Input({ required: true }) title: string = '';
+  @Input({ transform: booleanAttribute }) withShadow: boolean = false;
+}
