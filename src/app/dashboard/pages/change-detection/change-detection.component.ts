caf5b0f41084fe00f611ad67ef4e7b71756aea1e
Rocio Garrido <rocio.garrido@wefox.com>
2024-05-10 15:38:35 +0200

    feat: ChangeDetection - OnPush

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,12 +1,51 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from "@shared/title/title.component";
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './change-detection.component.html',
-  styles: ``
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [CommonModule, TitleComponent],
+  template: `
+  
+  <app-title [title]="currentFramework()" />
+  
+  <pre> {{ frameworksAsSignal() | json }}</pre>
+  <pre> {{ frameworksAsProperty | json }}</pre>
+  
+  `
 })
 export default class ChangeDetectionComponent {
+
+  public currentFramework = computed(
+    () => `Change detection - ${this.frameworksAsSignal().name}`
+  )
+
+  public frameworksAsSignal = signal({
+    name: 'Angular',
+    releaseDate: 2016,
+  });
+
+  public frameworksAsProperty = {
+    name: 'Angular',
+    releaseDate: 2016,
+  };
+
+  constructor() {
+
+    setTimeout(() => {
+
+      // this.frameworksAsProperty.name = 'React';
+
+      this.frameworksAsSignal.update(value => {
+        value.name = 'React';
+
+        return { ...value };
+      })
+
+      console.log('Hecho');
+
+    }, 3000);
+  }
 
 }
