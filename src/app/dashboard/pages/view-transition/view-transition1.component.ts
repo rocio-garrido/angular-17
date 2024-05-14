e78c2d012bdc41837346345cded34e536b640c3f
Rocio Garrido <rocio.garrido@wefox.com>
2024-05-14 12:34:50 +0200

    Hero Animation - View Transitions

--- src/app/dashboard/pages/view-transition/view-transition1.component.ts
+++ src/app/dashboard/pages/view-transition/view-transition1.component.ts
@@ -19,9 +19,12 @@ import { TitleComponent } from '@shared/title/title.component';
     alt="Picsum"
     width="200"
     height="300"
+    style="view-transition-name: hero1"
     />
 
-    <div class="bg-blue-500 w-56 h-56">
+    <div 
+    class="bg-blue-500 w-56 h-56"
+    style="view-transition-name: hero2">
 
     </div>
   </section>
