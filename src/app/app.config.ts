d5d116740fc097fa97f2032d477eec626e46fa6e
Rocio Garrido <rocio.garrido@wefox.com>
2024-05-15 11:20:36 +0200

    Peticiones HTTP - importProvidersFrom

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,7 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter, withViewTransitions } from '@angular/router';
 
+import { HttpClientModule } from '@angular/common/http';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
@@ -14,5 +15,9 @@ export const appConfig: ApplicationConfig = {
         }
       })
     ),
+
+    importProvidersFrom(
+      HttpClientModule,
+    )
   ]
 };
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,5 +1,7 @@
-import { Injectable, signal } from '@angular/core';
-import { User } from '@interfaces/req-response';
+import { HttpClient } from '@angular/common/http';
+import { Injectable, computed, inject, signal } from '@angular/core';
+import { User, UsersResponse } from '@interfaces/req-response';
+import { delay } from 'rxjs';
 
 interface State {
   users: User[];
@@ -11,13 +13,25 @@ interface State {
 })
 export class UsersService {
 
+  private http = inject(HttpClient);
+
   #state = signal<State>({
     loading: true,
     users: [],
-  })
+  });
+
+  public users = computed(() => this.#state().users);
+  public loading = computed(() => this.#state().loading);
 
   constructor() {
-    console.log('cargando data')
+    this.http.get<UsersResponse>('https://reqres.in/api/users')
+      .pipe(delay(1000))
+      .subscribe(res => {
+        this.#state.set({
+          loading: false,
+          users: res.data,
+        })
+      })
   }
 
 }
